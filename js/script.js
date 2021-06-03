ba32554bb83823a9b49a57e9ab2c915d2666c260
gerlinp <gerlin.pierrelouis@gmail.com>
2021-06-02 22:56:57 -0400

    getRandomQuote and printquote function

--- js/script.js
+++ js/script.js
@@ -11,17 +11,76 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let refresh = [];
+let pop;
+let quote;
+let quotes = [
+  { 
+    quote: 'It’s not a bug. It’s an undocumented feature!',
+    author:'Anonymous'
+  },
+  { 
+    quote: 'A website without visitors is like a ship lost in the horizon',
+    author:'Dr. Christopher Dayagdag'
+  },
+  { 
+    quote: 'A website without visitors is like a ship lost in the horizon',
+    author:'Dr. Christopher Dayagdag'
+  },
+  { 
+    quote: 'If debugging is the process of removing software bugs, then programming must be the process of putting them in',
+    author:'Edsger Dijkstra'
+  },
+  { 
+    quote: 'If you think math is hard, try web design.',
+    author:'Trish Parr'
+  },
+  { 
+    quote: '“A user interface is like a joke. If you have to explain it, it’s not that good.”',
+    author:'Unknown'
+  },
+  
+  { 
+    quote: '“The public is more familiar with bad design than good design. It is, in effect, conditioned to prefer bad design, because that is what it lives with. The new becomes threatening, the old reassuring.”',
+    author:'Paul Rand'
+  },
+  { 
+    quote: '“The public is more familiar with bad design than good design. It is, in effect, conditioned to prefer bad design, because that is what it lives with. The new becomes threatening, the old reassuring.”',
+    author:'Paul Rand'
+  },
+  
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  let random = Math.floor(Math.random() * quotes.length);
+  return quotes[random]
+}
 
 
 /***
  * `printQuote` function
 ***/
+
+let quoteBox = document.querySelector('#quote-box');
+function printQuote() {
+  quote = getRandomQuote();
+  let randomQuote = `<p class="quote">${quote.quote}</p>
+  <p class="source">${quote.author}`;
+  if (quote.hasOwnProperty('citation')) {
+    randomQuote += `<span class="citation">${quote.citation}</span>`;
+  }
+  if (quote.hasOwnProperty('year')) {
+    randomQuote +=`<span class="year">2016</span>`;
+  }
+  randomQuote += `</p>`
+  quoteBox.innerHTML = randomQuote;
+}
+
+printQuote();
 
 
 
@@ -30,4 +89,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
